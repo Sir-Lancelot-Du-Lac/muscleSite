d12849a9b4290cfea335122603fffc6477049fe6
Sir-Lancelot-Du-Lac <lionel.feter@wp.pl>
2023-03-21 21:19:23 +0100

    Working contact panel Blured  background and footer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,25 @@
 import React from 'react'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export default function Contact() {
+
+  const handleCopy = (object) =>{
+    if(object==1){
+      console.log("copying")
+      navigator.clipboard.writeText("lionel.feter@wp.pl")
+    }
+  }
+
   return (
-    <div>Contact</div>
+    <div>
+      <div className={'${stylescss.bodyClass} bodyClass'}>
+        <div className={'${stylescss.ContainerMain} ContainerMain'}>
+            <h1>Contact</h1>
+            <a href='https://github.com/Sir-Lancelot-Du-Lac'><i className='bi bi-github 300px ${stylescss.iconSize} iconSize' /></a>
+            <a href='https://www.linkedin.com/in/jarema-feter-947b35269/'><i className="bi bi-linkedin ${stylescss.iconSize} iconSize" /></a>
+            <a><i className="bi bi-envelope-at ${stylescss.iconSize} iconSize" onClick={() => handleCopy(1)} /></a>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -16,6 +16,7 @@ import Exercises from '../Exercises/Exercises.js'
 export default function Main() {
   return (
     <div>
+      <div className={'${stylescss.blurBc} blurBc'}></div>
       <Router>
         <div className={'${stylescss.NavContainer} NavContainer'}>
             <Link to="/" className={'${stylescss.NavItems} NavItems'}>Home</Link>
@@ -24,14 +25,20 @@ export default function Main() {
             <Link to="/NavYou" className={'${stylescss.NavItems} NavItems'}>Your training plan</Link>
             <Link to="/NavCon" className={'${stylescss.NavItems} NavItems'}>Contact</Link>
         </div>
+        <div className={'${stylescss.TextContainer} TextContainer'}>
           <Routes>
-            <Route path="/" element={Home}/>
-            <Route path="/NavMus" element={Muscle}/>
-            <Route path="/NavExa" element={Exercises}/>
-            <Route path="/NavYou" element={OwnTrainingPlan}/>
-            <Route path="/NavCon" element={Contact}/>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/NavMus" element={<Muscle/>}/>
+            <Route path="/NavExa" element={<Exercises/>}/>
+            <Route path="/NavYou" element={<OwnTrainingPlan/>}/>
+            <Route path="/NavCon" element={<Contact/>}/>
           </Routes>
+        </div>
         </Router>
+        <div id="footer">
+          Hello <br/>
+          It's my site :)
+        </div>
     </div>
   )
 }
